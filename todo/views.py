2027d2410cdc556fde6054f157ed5f88dbe5dec3
Kübra <kubranur216@gmail.com>
2023-03-20 12:53:20 +0300

    commit

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+
+from .models import Todo
+from .serializers import TodoSerializer
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def todo_list_create(request):
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
